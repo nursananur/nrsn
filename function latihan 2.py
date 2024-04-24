1fad7ec307114402275c9288546d6bc88d858a32
Nursan19 <163141594+nursananur@users.noreply.github.com>
2024-04-24 04:52:57 -0700

    Add files via upload

---
+++ function latihan 2.py
@@ -0,0 +1,8 @@
+name = input("Masukkan Nama: ")
+capter = input("Masukkan Latihan ke: ")
+
+def sorts():
+    print("Nama Saya:", name)
+    print("Saya sedang mengerjakan latihan soal:", capter)
+
+sorts()
